aa8bff1ccf06d7a5315456cbf9ecc593bc4ae1ec
zehaochen19 <zehaochen19@gmail.com>
2018-12-26 09:23:35 +0800

    update Main

--- src/visualizer/app/Main.hs
+++ src/visualizer/app/Main.hs
@@ -1,22 +1,28 @@
 module Main where
 
-import           Web.Scotty                           (middleware, scotty)
-import           Control.Monad                        (join)
-import           Control.Applicative                  ((<$>))
-import           Controllers.Home                     (index)
-import           Data.Maybe                           (fromMaybe)
-import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import           Network.Wai.Middleware.Static        (addBase, noDots,
-                                                        staticPolicy, (>->))
-import           System.Environment                   (lookupEnv)
-import           Text.Read                            (readMaybe)
+import           Web.Scotty                     ( middleware
+                                                , scotty
+                                                )
+import           Control.Monad                  ( join )
+import           Control.Applicative            ( (<$>) )
+import           Controllers.Home               ( index
+                                                , postJava
+                                                )
+import           Data.Maybe                     ( fromMaybe )
+import           Network.Wai.Middleware.RequestLogger
+                                                ( logStdoutDev )
+import           Network.Wai.Middleware.Static  ( addBase
+                                                , noDots
+                                                , staticPolicy
+                                                , (>->)
+                                                )
+import           System.Environment             ( lookupEnv )
+import           Text.Read                      ( readMaybe )
 
 main :: IO ()
 main = do
-    port <- fromMaybe 3000
-        . join
-        . fmap readMaybe <$> lookupEnv "PORT"
+    port <- fromMaybe 3000 . join . fmap readMaybe <$> lookupEnv "PORT"
     scotty port $ do
-            middleware $ staticPolicy (noDots >-> addBase "static") -- for static sources
-            middleware logStdoutDev
-            index
+        middleware $ staticPolicy (noDots >-> addBase "static") -- for static sources
+        middleware logStdoutDev
+        index >> postJava
